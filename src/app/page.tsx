340b0ed28a86156e958d2c1f05df6bf26f582b11
NabilDia <mohamed-nabil.dia.edu@groupe-gema.com>
2024-11-13 13:48:54 +0100

    Menu de navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,53 @@
+'use client'
+import React from 'react';
+import Menu from './components/navbar';
 
-export default function Home() {
+export default function page() {
+  // Définissez les fonctions onClick pour chaque élément du menu
+  const handleNewDropClick = () => {
+    alert('Naviguez vers la section New Drop.');
+  };
+  const handleCollectionClick = () => {
+    alert('Naviguez vers la section Collections.');
+  };
+  const handleSalesClick = () => {
+    alert('Naviguez vers la section Sales.');
+  };
+  const handleGiftCardsClick = () => {
+    alert('Naviguez vers les Gift Cards.');
+  };
+  const handleBrandsClick = () => {
+    alert('Naviguez vers les Brands.');
+  };
+  const handleMagazineClick = () => {
+    alert('Naviguez vers les Magazines.');
+  };
+  const handleAboutUsClick = () => {
+    alert('Naviguez vers la section À propos de nous.');
+  };
+  const handleContactUsClick = () => {
+    alert('Naviguez vers la section Contactez-nous.');
+  };
+  
+ 
+
+  // Définissez les éléments de menu
+  const menuItems = [
+    
+    { label: 'Nouveaux produits', onClick: handleNewDropClick },
+    { label: 'Collections', onClick: handleCollectionClick },
+    { label: 'Ventes', onClick: handleSalesClick },
+    { label: 'Cartes cadeaux', onClick: handleGiftCardsClick },
+    { label: 'Marques', onClick: handleBrandsClick },
+    { label: 'Magazine', onClick: handleMagazineClick },
+    { label: 'À propos de nous', onClick: handleAboutUsClick },
+    { label: 'Contactez-nous', onClick: handleContactUsClick },
+  ];
+
   return (
-    <div>Hello world</div>
+    <div>
+      <Menu items={menuItems} backgroundColor="white" />
+      {/* Ajoutez ici d'autres composants ou contenu de la page */}
+    </div>
   );
 }
